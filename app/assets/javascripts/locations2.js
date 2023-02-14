7e7698dd44e9ae421b539b7a0b7c9d9d962c48ca
ismail-co <ismail.ismail@cabinetoffice.gov.uk>
2023-02-14 09:03:12 +0000

    Fixed change links
    Change links redirect back to the summary page

--- app/assets/javascripts/locations2.js
+++ app/assets/javascripts/locations2.js
@@ -5,7 +5,17 @@ var locations2 = [
   "6 Central Square, Cardiff CF10 1EP",
   "3 Glass Wharf, Bristol BS2 0EL",
   "3 Arena Central, Bridge St, Birmingham B1 2AX"
+  "19 Saint Johns Road, Northampton NN91 9NY",
+  "52 Main Road, Darlington DL0 8CM",
+  "149 Broadway, Milton Keynes MK92 0PC",
+  "53 The Avenue, Hereford HR73 2IW",
+  "6 Mill Road, Edinburgh EH64 1ZE",
+  "87 Church Lane, Lincoln LN43 7LP",
+  "25 The Crescent, Cardiff CF10 1XC",
+  "5 Victoria Street, Kingston Upon Thames KT37 2JH",
+  "65 Norwood Rd, Herne Hill, London SE24 9AA"
   ]
   var element = document.querySelector('#locations2')
   var id = 'autocomplete-default'
   accessibleAutocomplete({element: element, id: id, source: locations2})
+  
--- app/routes.js
+++ app/routes.js
@@ -2,17 +2,251 @@ const express = require('express')
 const router = express.Router()
 
 
+
 router.post("/basic-details-handler", function (req, res) {
-
-if (req.session.data['summary'] = "true") {
-res.redirect('new-campaign-set-up/summary-section-1')
-}
-else {
-  res.redirect('new-campaign-set-up/job-information')
-
-}
+	if (req.session.data['edit1'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-1')
+	} else {
+		res.redirect('new-campaign-set-up/job-information')
+	}
 })
 
+router.post("/locations-handler", function (req, res) {
+	var remote = req.session.data['uk']
+	if (req.session.data['edit2'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-2')
+	} else if (remote == "Remote") {
+		res.redirect('new-campaign-set-up/contract-details')
+	} else {
+		res.redirect('new-campaign-set-up/locations-2')
+	}
+})
+
+router.post("/locations-2-handler", function (req, res) {
+	if (req.session.data['edit3'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-2')
+	} else {
+		res.redirect('new-campaign-set-up/contract-details')
+	}
+})
+
+router.post("/contract-details-handler", function (req, res) {
+	if (req.session.data['edit4'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-2')
+	} else {
+		res.redirect('new-campaign-set-up/grades')
+	}
+})
+
+router.post("/salary-handler", function (req, res) {
+	if (req.session.data['edit5'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-2')
+	} else {
+		res.redirect('new-campaign-set-up/benefits')
+	}
+})
+
+router.post("/benefits-handler", function (req, res) {
+	if (req.session.data['edit6'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-2')
+	} else {
+		res.redirect('new-campaign-set-up/contact-details')
+	}
+})
+
+router.post("/contact-details-handler", function (req, res) {
+	if (req.session.data['edit7'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-2')
+	} else {
+		res.redirect('new-campaign-set-up/contact-details-2')
+	}
+})
+
+router.post("/contact-details-2-handler", function (req, res) {
+	if (req.session.data['edit8'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-2')
+	} else {
+		res.redirect('new-campaign-set-up/reserved-status')
+	}
+})
+
+router.post("/about-the-role-handler", function (req, res) {
+	if (req.session.data['edit9'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-3')
+	} else {
+		res.redirect('new-campaign-set-up/essential-criteria')
+	}
+})
+
+
+router.post("/essential-criteria-handler", function (req, res) {
+	if (req.session.data['edit10'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-3')
+	} else {
+		res.redirect('new-campaign-set-up/tech-criteria')
+	}
+})
+
+
+router.post("/tech-criteria-handler", function (req, res) {
+	if (req.session.data['edit11'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-3')
+	} else {
+		res.redirect('new-campaign-set-up/pre-sift-questions')
+	}
+})
+
+
+
+router.post("/pre-sift-questions-handler", function (req, res) {
+	if (req.session.data['edit12'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-3')
+	} else {
+		res.redirect('new-campaign-set-up/additional-info')
+	}
+})
+
+router.post("/how-sift-handler", function (req, res) {
+	if (req.session.data['edit13'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-4')
+	} else {
+		res.redirect('new-campaign-set-up/interview-evidence')
+	}
+})
+
+router.post("/interview-evidence-handler", function (req, res) {
+	if (req.session.data['edit14'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-4')
+	} else {
+		res.redirect('new-campaign-set-up/experience')
+	}
+})
+
+
+router.post("/experience-handler", function (req, res) {
+	if (req.session.data['edit15'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-4')
+	} else {
+		res.redirect('new-campaign-set-up/behaviours')
+	}
+})
+
+
+router.post("/technical-handler", function (req, res) {
+	if (req.session.data['edit16'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-4')
+	} else {
+		res.redirect('new-campaign-set-up/other-methods')
+	}
+})
+
+
+router.post("/online-tests-handler", function (req, res) {
+	if (req.session.data['edit17'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-4')
+	} else {
+		res.redirect('new-campaign-set-up/experience')
+	}
+})
+
+
+router.post("/behaviours-handler", function (req, res) {
+	if (req.session.data['edit18'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-4')
+	} else {
+		res.redirect('new-campaign-set-up/sift-or-interview')
+	}
+})
+
+
+
+router.post("/sift-or-interview-handler", function (req, res) {
+	if (req.session.data['edit19'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-4')
+	} else {
+		res.redirect('new-campaign-set-up/strengths-change')
+	}
+})
+
+router.post("/strengths-change-handler", function (req, res) {
+	if (req.session.data['edit20'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-4')
+	} else {
+		res.redirect('new-campaign-set-up/technical')
+	}
+})
+
+router.post("/approach-handler", function (req, res) {
+	if (req.session.data['edit21'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-5')
+	} else {
+		res.redirect('new-campaign-set-up/approval')
+	}
+})
+
+
+
+
+
+router.post("/approval-handler", function (req, res) {
+	if (req.session.data['edit22'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-5')
+	} else {
+		res.redirect('new-campaign-set-up/approval-upload')
+	}
+})
+
+
+
+router.post("/approval-2-handler", function (req, res) {
+	if (req.session.data['edit'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-5')
+	} else {
+		res.redirect('new-campaign-set-up/reserve-list')
+	}
+})
+
+
+router.post("/reserve-list-handler", function (req, res) {
+	if (req.session.data['edit23'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-5')
+	} else {
+		res.redirect('new-campaign-set-up/attraction-methods')
+	}
+})
+
+router.post("/attraction-handler", function (req, res) {
+	if (req.session.data['edit24'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-5')
+	} else {
+		res.redirect('new-campaign-set-up/summary-section-5')
+	}
+})
+
+
+router.post("/interviews-handler", function (req, res) {
+	if (req.session.data['edit25'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-6')
+	} else {
+		res.redirect('new-campaign-set-up/campaign-dates')
+	}
+})
+
+router.post("/campaign-dates-handler", function (req, res) {
+	if (req.session.data['edit26'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-6')
+	} else {
+		res.redirect('new-campaign-set-up/pre-submit-reviewer')
+	}
+})
+
+router.post("/pre-submit-reviewer-handler", function (req, res) {
+	if (req.session.data['edit27'] == "true") {
+		res.redirect('new-campaign-set-up/summary-section-6')
+	} else {
+		res.redirect('new-campaign-set-up/submit')
+	}
+})
 // router.post("/job-information-handler", function (req, res) {
 //   var checked = req.session.data['answers-checked']
 //
